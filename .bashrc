e7cb2e3f2d051a38f5fa3e97311dfc08178a1447
xenomech <gokulsmenon227@gmail.com>
2021-05-17 01:55:06 +0530

    ✨ 3 prompts added

--- .bashrc
+++ .bashrc
@@ -5,3 +5,4 @@
 export PS1="[ \u @ \h \W ] [ \d - \@ ] \n~> " # Plain - no addons
 
 export PS1=" 📅 \d - 🕡 \@ \n 👨‍ \u @ 🛰️ \h : 🌏 \W  \n ⚡ " #3 line emoji
+
--- install.sh
+++ install.sh
@@ -3,35 +3,35 @@
 echo "installing" $1
 echo "Proceed ? (y/n)"
 
-add_addon(){
-    
+read $lock
+
+add_addon_git() {
+    echo -e "function gitbranch() { \n git branch 2>/dev/null | grep '^*' | colrm 1 2 | sed -e 's/^/🌲 /' \n }" >>~/.bashrc
 }
 
-read $lock
+add_addon_status() {
+    echo -e "\n exitstatus() { \n if [[ $? == 0 ]]; then \n\t echo '🤓' \n else \n\t echo '💩'\n fi \n }" >>~/.bashrc
+}
 
 if [[ $lock -eq "y" ]]; then
     echo "backing up the current .bashrc as .bashrc.bak"
     cp ~/.bashrc ~/.bashrc.bak
     if [[ $1 == "p" ]]; then
-
         new_prompt='[ \u @ \h \W ] [ \d - \@ ] \n~> ' # Plain - no addons
-
     elif
         [[ $1 == "e" ]]
     then
-
-        new_prompt='📅 \d - 🕡 \@ \n👨‍ \u @ 🛰️ \h : 🌏 \W  \n⚡ ' # Emoji - no addons
-
-    fi
+        new_prompt='\n📅 \d - 🕡 \@ \n👨‍ \u @ 🛰️ \h : 🌏 \W  \n⚡ ' # Emoji - no addons
     elif
         [[ $1 == "ex" ]]
     then
-
-        new_prompt='📅 \d - 🕡 \@ \n👨‍ \u @ 🛰️ \h : 🌏 \W  \n⚡ ' # Emoji - no addons
-
+        {
+            add_addon_git
+            new_prompt='\n📅 \d - 🕡 \@ \n👨‍ \u @ 🛰️ \h : 🌏 \W $(gitbranch) $(exitstatus) \n⚡ ' # Emoji - no addons
+            add_addon_status
+        }
+        sed -i '/^export PS1/s//#&/' ~/.bashrc
+        echo "# code injected by Rocket prompt" >>~/.bashrc
+        echo "export PS1='$new_prompt'" >>~/.bashrc
     fi
-
-    sed -i '/^export PS1/s//#&/' ~/.bashrc
-    echo "# code injected by Rocket prompt" >>~/.bashrc
-    echo "export PS1='$new_prompt'" >>~/.bashrc
 fi
